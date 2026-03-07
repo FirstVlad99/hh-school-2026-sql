8e7f55fa70de57d516c379f38ef8946d0823c27a
Vladislav Pankov <vladislw.pankov@gmail.com>
2026-03-08 00:02:00 +0300

    Add files from feature_one

---
+++ tasks/task_1.sql
@@ -0,0 +1,77 @@
+CREATE TABLE region
+(
+    region_id integer generated always as identity primary key,
+    name      text not null
+);
+
+CREATE TABLE employer
+(
+    employer_id  integer generated always as identity primary key,
+    name         text        not null,
+    phone_number varchar(16) not null,
+    created_at   timestamp default now(),
+    region_id    integer references region (region_id)
+);
+CREATE TABLE applicant
+(
+    applicant_id integer generated always as identity primary key,
+    first_name   text        not null,
+    second_name  text        not null,
+    middle_name  text        not null,
+    birth_date   date,
+    phone_number varchar(16) not null,
+    created_at   timestamp default now(),
+    region_id    integer references region (region_id)
+);
+
+CREATE TABLE vacancy
+(
+    vacancy_id        integer generated always as identity primary key,
+    position_name     text not null,
+    compensation_from integer,
+    compensation_to   integer,
+    created_at        timestamp default now(),
+    employer_id       integer references employer (employer_id),
+    region_id         integer references region (region_id)
+);
+
+CREATE TABLE resume
+(
+    resume_id         integer generated always as identity primary key,
+    position_name     text not null,
+    compensation_from integer,
+    compensation_to   integer,
+    created_at        timestamp default now(),
+    applicant_id      integer references applicant (applicant_id),
+    region_id         integer references region (region_id)
+);
+CREATE TYPE response_status AS ENUM ('pending', 'submitted', 'reviewed', 'accepted', 'rejected');
+
+CREATE TABLE response
+(
+    response_id integer generated always as identity primary key,
+    status      text      default 'submitted',
+    created_at  timestamp default now(),
+    vacancy_id  integer references vacancy (vacancy_id),
+    resume_id   integer references resume (resume_id)
+);
+
+CREATE TABLE industry
+(
+    industry_id integer generated always as identity primary key,
+    name        text not null
+);
+
+CREATE TABLE specialization
+(
+    specialization_id integer generated always as identity primary key,
+    name              text not null,
+    industry_id       integer references industry (industry_id)
+);
+
+CREATE TABLE resume_specializations
+(
+    resume_id         integer not null references resume (resume_id),
+    specialization_id integer not null references specialization (specialization_id),
+    PRIMARY KEY (resume_id, specialization_id)
+);
---
+++ tasks/task_5.sql
@@ -0,0 +1,8 @@
+SELECT v.vacancy_id          "ID Вакансии",
+       v.position_name    as Заголовок,
+       count(response_id) as Количество
+FROM vacancy as v
+         JOIN response r using (vacancy_id)
+WHERE r.created_at BETWEEN v.created_at AND v.created_at + interval '1 week'
+GROUP BY v.vacancy_id, v.position_name
+HAVING count(response_id) > 5;
